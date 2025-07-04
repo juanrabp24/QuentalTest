d62d13033251a512e7787edd0bf96d76770dfb3e
juanrabp24 <juanrabp24@gmail.com>
2025-07-04 17:54:48 +0200

    Modificar presupuesto por terminar controlador

--- src/Service/Clubes/Modificar/ModificarPresupuestoService.php
+++ src/Service/Clubes/Modificar/ModificarPresupuestoService.php
@@ -2,7 +2,51 @@
 
 namespace App\Service\Clubes\Modificar;
 
+use App\Entity\Clubes;
+use App\Entity\Entrenadores;
+use Doctrine\ORM\EntityManagerInterface;
+
 class ModificarPresupuestoService
 {
+    public function __construct(private EntityManagerInterface $em){}
+
+
+    public function modificarPresupuesto(array $data): Clubes
+    {
+        if (empty($data['club'])) {
+            throw new \InvalidArgumentException('Se necesitan las id del club para poder modificar el presupuesto');
+        }
+        $salario = 0;
+
+        $club = $this->em->getRepository(Clubes::class)->find($data['club']);
+
+        if (is_null($club)) {
+            throw new \Exception('El club no existe', 200);
+        }
+
+        if(!$club->getEntrenadores()->isEmpty()){
+            foreach ($club->getEntrenadores() as $entrenador) {
+                $salario += $entrenador->getSalario();
+            }
+        }
+
+        if(!$club->getJugadores()->isEmpty()){
+            foreach ($club->getJugadores() as $jugador) {
+                $salario += $jugador->getSalario();
+            }
+        }
+
+        if ($data['presupuesto'] < $salario) {
+            throw new \InvalidArgumentException('El salario de los jugadores y entrenadores es mayor que el presupuesto');
+        }
+
+        $club->setPresupuesto($data['presupuesto']);
+
+        $this->em->persist($club);
+        $this->em->flush();
+
+        return $club;
+
+    }
 
 }
